92c5cae8598602e999e8cf90fad4653a06f02bc1
LarissaGiovanna <lgiova544@gmail.com>
2026-02-03 12:59:17 -0300

    feat: codigo esp principal servomotores - ideia inicial do codigo testando com monitor serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
-#include <Arduino.h>
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
 
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
+*/
+
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo myservo;  // create Servo object to control a servo
+// twelve Servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
+  myservo.attach(26);  // attaches the servo on pin 9 to the Servo object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("LED is on");
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("LED is off");
-  delay(1000);
+  int num = Serial.read(); //substituir pela leitura do esp now vinda da outra placa
+  if (num == 1){
+    myservo.write(0); 
+    delay(100);
+    myservo.write(180);
+    delay(105);
+    myservo.write(90); //parado
+    delay(1000);
+  }else if (num == 2){
+    myservo.write(180); 
+    delay(100);
+    myservo.write(0);
+    delay(105);
+    myservo.write(90); //parado
+    delay(1000);
+  }
 }
